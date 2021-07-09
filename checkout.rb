669e4349a9a09d14bbc3267f16c2d5655d693f19
Kntanos <86077849+Kntanos@users.noreply.github.com>
2021-07-09 15:34:38 +0100

    Commit final changes to exercises

--- checkout.rb
+++ checkout.rb
@@ -1,21 +1,20 @@
-class Checkout
+class Basket
     attr_accessor :list
     def initialize()
-        @list = {}
+        @list ={}
     end
 
     def add_item(item)
         list[item.name] = item.price
-        puts "You have added #{item.name}, for £#{'%.2f' % item.price}, to your basket"
+        puts "You have added #{item.name}, for £#{'%.2f' % item.price}, to your basket."
     end
 
     def total
         total = list.values.sum
-        puts "Your total so far is £#{'%.2f' % total}."
-    end
+    end 
 end
 
-class Item
+class Product
     attr_accessor :name, :price
     def initialize(name, price)
         @name = name
@@ -23,29 +22,22 @@ class Item
     end
 end
 
-sausages = Item.new("sausages", 5.00)
-milk = Item.new("milk", 3.00)
-
-checkout = Checkout.new()
-
-checkout.add_item(milk)
-checkout.add_item(sausages)
-checkout.total
-
-=begin
-class Payment
-    attr_accessor :card, :payment_due
-    def initialize(card, payment_due)
+class Checkout
+    attr_accessor :basket_total, :card
+    def initialize(basket_total)
+        @basket_total = basket_total
         @card = card
-        @payment_due = payment_due
     end
 
-    def balance()
-        new_balance = card.credit - payment_due
-        puts "Your remaining credit is £#{'%.2f' % new_balance}." 
+    def payment_due()
+        puts "Your total to pay is £#{'%.2f' % basket_total.total}."
+    end
+
+    def pay(card)
+        puts "You have £#{'%.2f' % card.credit} on your card."
+        puts "Your new balance is £#{'%.2f' % (card.credit - basket_total.total)}."
     end
 end
-
 
 class Card
     attr_accessor :credit
@@ -54,8 +46,15 @@ class Card
     end
 end
 
-card = Card.new(100.00)
-payment = Payment.new(card, 8.00)
+basket = Basket.new()
+sausages = Product.new("sausages", 5.00)
+milk = Product.new("milk", 3.00)
 
-payment.balance()
-=end
+basket.add_item(milk)
+basket.add_item(sausages)
+basket.total
+checkout = Checkout.new(basket)
+checkout.payment_due()
+
+card = Card.new(10.00)
+checkout.pay(card)
--- ordering.rb
+++ ordering.rb
@@ -1,4 +1,69 @@
-require "mail"
+class Shop
+    attr_accessor :basket, :stock
+    def initialize()
+        #@basket = Basket.new()
+        @stock = [
+            {name: "Water Bottle", price: 8.00, stock_count: 5},
+            {name: "Cheese Grater", price: 15.00, stock_count: 1},
+          ]
+    end
+end
+
+class Basket
+    attr_accessor :basket, :shop_stock, :shop
+    def initialize()
+        @shop = Shop.new()
+        @shop_stock = shop.stock
+        @basket = []
+    end
+    
+    def add_to_basket(product)
+        check_in_stock(product)
+    end
+    
+    def item_exist?(product)
+        shop_stock.any?{ |item| item[:name] == product}
+    end
+    
+    def check_in_stock(product)
+        return false unless item_exist?(product) # this will return prematurely if the item doesnt exist
+        if shop_stock.any?{ |item| item[:name] == product && item[:stock_count] >=1}
+            add_item(product)
+        end
+    end  
+
+    def add_item(product)
+        @basket << shop_stock.select{ |item| item[:name] == product && item[:stock_count] >=1}
+        puts "You have added #{product} to your basket."
+    end
+
+    def basket_total
+        total = basket.flatten.map{|item| item[:price]}.sum
+    end
+end
+
+class Checkout
+    attr_accessor :total, :card
+    def initialize(total)
+        @total = total
+        @card = card
+    end
+
+    def pay(card)
+        puts "Your total is £#{'%.2f' % total}."
+        if card.credit < total
+            puts "You don't have sufficient funds!"
+        else puts "Your new balance is £#{'%.2f' % (card.credit - total)}."
+        end
+    end
+end
+
+class CreditCard 
+    attr_accessor :credit
+    def initialize(credit)
+        @credit = credit
+    end
+end
 
 class User
     attr_accessor :name, :email, :address, :credit_card
@@ -10,60 +75,11 @@ class User
     end
 end
 
-class CreditCard
-    attr_accessor :credit
-    def initialize(user)
-        @credit = user.credit_card
-    end
-end
-
-class Shop
-    attr_accessor :basket, :items, :name
-    def initialize(name)
-        @name = name
-        @basket = []
-        @items = [
-        {name: "Water Bottle", price: 8.00, stock_count: 5},
-        {name: "Cheese Grater", price: 15.00, stock_count: 1},
-      ]
-    end
-
-    def add_item_to_basket(product)
-        unless (items.map{ |item| item.value?(product)}).include?(true) 
-            puts "Product not found!"
-        end
-        if items.map{ |item| item.value?(product) && item[:stock_count] >=1}.include?(true)
-            basket << items.select{ |item| item.value?(product)}
-            puts "You have added #{product} to your #{name} basket."
-        else puts "The product is out of stock!"
-        end
-    end
-
-    def checkout(card)
-        #list = basket.flatten.map{ |item| item[:name]}
-        puts "Your total is £#{'%.2f' % total}"
-        puts "You have £#{'%.2f' % card.credit} in your card."
-        puts "Your new balance is £#{'%.2f' % (card.credit - total)}."
-    end
-
-    def total
-       total = basket.flatten.map{|item| item[:price]}.sum
-    end
-end
-
-user_1 = User.new("Kostas", "kostasntanos@hotmail.com", "2 Road Street", 25.00)
-card = CreditCard.new(user_1)
-shop = Shop.new("Amazon")
-shop.add_item_to_basket("Water Bottle")
-shop.add_item_to_basket("Cheese Grater")
-shop.checkout(card)
-
-=begin
-mail = Mail.new do
-    from    "kostasntanos@hotmail.com"
-    to      "kostasntanps@hotmail.com"
-    subject "Your Amazon Order... not really"
-    body    "body"
-  end
-  mail.deliver!
-=end
+shop = Shop.new()
+user = User.new("Kostas", "kostasntanos@hotmail.com", "2 Road Street", 25.00)
+basket = Basket.new()
+basket.add_to_basket("Water Bottle")
+basket.add_to_basket("Cheese Grater")
+checkout = Checkout.new(basket.basket_total) # It is better to give specific variables than whole objects, so that information that is not needed, does nto become accessible.
+card = CreditCard.new(user.credit_card)
+checkout.pay(card)
